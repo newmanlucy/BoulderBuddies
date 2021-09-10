b3721f37af2bcf1c88b5879a5efa0152f56797c8
Lucille Newman <Lucy.Newman@ibm.com>
2021-09-10 19:10:06 -0400

    fixed bug in register page

--- boulder-buddies-frontend/src/components/register.jsx
+++ boulder-buddies-frontend/src/components/register.jsx
@@ -6,9 +6,9 @@ class Register extends Component {
 
   constructor(props){
     super(props)
-    this.rootUrl = "http://localhost:8000";
+    // this.rootUrl = "http://localhost:8000";
+    this.rootUrl = "http://34.125.244.56";
     this.endpoint = "/users/";
-    this.props.uid = 1;
     this.url = this.rootUrl + this.endpoint;
     this.state = {username: "", level: "0", location: "", bio: "", gender: false}
     this.handleChange = this.handleChange.bind(this);
